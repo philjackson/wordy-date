7ed08f100d1bbca455cb16eb8c297138e4e1dc84
Phil Jackson <phil@shellarchive.co.uk>
2015-12-29 18:09:58 +0000

    Add the basic parsers.

--- src/wordy_date/core.clj
+++ src/wordy_date/core.clj
@@ -1,12 +1,35 @@
 (ns wordy-date.core
   (:require [clojure.string :as str]
-            [clj-time.core :as time]
+            [clj-time.core :as t]
             [instaparse.core :as insta]))
 
 (def wordy-date-parser (insta/parser
-                        (str/join "\n" ["whitespace = #'\\s+'"
-                                        "digits = #'-?[0-9]+'"])
+                        (str/join "\n" ["S = duration | dow | quickie"
+                                        "quickie = 'tomorrow' | 'now'"
+                                        "duration = (<pre-superfluous> <whitespace>)? digits <whitespace> period"
+                                        "period = #'(min(ute)?|day|hour|week|month|year)s?'"
+                                        "dow = (dow-modifier <whitespace>)? long-days"
+                                        "long-days = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday'"
+                                        "dow-modifier = 'next' | 'this'"
+                                        "<pre-superfluous> = 'in' | '+' | 'plus'"
+                                        "whitespace = #'\\s+'"
+                                        "digits = #'-?[0-9.]+'"])
                         :string-ci true))
 
 (defn parse [st]
-  )
+  (let [S (insta/transform {:digits clojure.edn/read-string
+                            :period #(case (subs % 0 3)
+                                       "min" t/minutes
+                                       "day" t/days
+                                       "hou" t/hours
+                                       "wee" t/weeks
+                                       "mon" t/months
+                                       "yea" t/years)
+                            :duration (fn [d p] (t/plus (t/now) (p d)))
+                            :quickie (fn [s]
+                                       (case s
+                                         "tomorrow" (t/plus (t/now) (t/days 1))
+                                         "now" (t/now)))}
+                           (wordy-date-parser st))]
+    (when (= (first #spy/p S) :S)
+      (second S))))
--- test/human_dates/core_test.clj
+++ test/human_dates/core_test.clj
@@ -1,10 +1,27 @@
 (ns wordy-date.core-test
   (:require [clojure.test :refer :all]
-            [clj-time.core :as time]
+            [clj-time.core :as t]
             [wordy-date.core :refer :all]))
 
 (deftest human-date-test
-  (let [time-now (time/date-time 2016 01 01)]
-    (with-redefs-fn {#'time/now (constantly time-now)}
-      #(testing "redef worked"
-         (is (= "2016-01-01T00:00:00.000Z" (str (time/now))))))))
+  (let [now (t/date-time 2016 10 11 12 13 14)
+        tomorrow (t/plus now (t/days 1))
+        match (fn [st date] (is (= (parse st) date)))]
+    (with-redefs-fn {#'t/now (constantly now)}
+      #(do
+         (testing "redef worked"
+           (is (= "2016-10-11T12:13:14.000Z" (str (t/now)))))
+
+         (testing "quickies"
+           (match "now" now)
+           (match "tomorrow" tomorrow))
+
+         (testing "periods"
+           (match "10 mins" (-> now (t/plus (t/minutes 10))))
+           (match "20 mins" (-> now (t/plus (t/minutes 20))))
+           (match "20 hours" (-> now (t/plus (t/hours 20))))
+           (match "28 hours" (-> now (t/plus (t/hours 28))))
+
+           (match "10.5 hours" (-> now
+                                   (t/plus (t/hours 28.5))
+                                   (t/plus (t/minutes 30)))))))))
