baa52a879787ebab2263384156133e9533e602c9
Phil Jackson <phil@shellarchive.co.uk>
2015-12-29 20:35:19 +0000

    Swap to the cljs version of instaparse.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [instaparse "1.4.1"]
+                 [com.lucasbradstreet/instaparse-cljs "1.4.1"]
                  [clj-time "0.11.0"]])
