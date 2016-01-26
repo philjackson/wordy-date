a1171405bfa781353c98e4ad36c5c7fdf8ffde8e
Phil Jackson <phil@shellarchive.co.uk>
2016-01-26 20:35:30 +0000

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject philjackson/wordy-date "0.1.6"
+(defproject philjackson/wordy-date "0.1.7"
   :description "A library that converts a date described as a human into an actual date."
   :url "https://github.com/philjackson/wordy-date"
   :license {:name "Eclipse Public License"
