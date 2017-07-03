a9a305f81eb420f83a4259aec4b2cebbbdf10426
Phil Jackson <phil@shellarchive.co.uk>
2017-07-03 14:44:25 +0100

    Use official instaparse.

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.andrewmcveigh/cljs-time "0.3.14"]
                  [org.clojure/clojurescript "1.7.145" :scope "provided"]
-                 [com.lucasbradstreet/instaparse-cljs "1.4.1.0"]
+                 [instaparse "1.4.7"]
                  [clj-time "0.11.0"]])
